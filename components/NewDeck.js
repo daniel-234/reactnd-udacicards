48804f6921703e514187df824a4f428c54c1621b
Daniele <daniele.rb6@gmail.com>
2017-09-27 13:03:20 +0200

    feat: Add a `submit` button to the `NewDeck` View

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, TouchableOpacity, Text } from 'react-native';
+
+// Create a lacal `SubmitButton` component.
+function SubmitBtn({ onPress }) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+    >
+      <Text>SUBMIT</Text>
+    </TouchableOpacity>
+  )
+}
+
+
 
 export default class NewDeck extends Component {
+	submit = (() => {
+		console.log('New Deck submitted')
+	})
+
 	render() {
 		return (
 			<View>
 				<Text>New Deck</Text>
+				<SubmitBtn
+					onPress={this.submit}
+				/>
 			</View>
-		);
+		)
 	}
-};
+}
