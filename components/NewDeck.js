73e463a9c22e3119ae8a77e06c0d0545d5c773f6
Daniele <daniele.rb6@gmail.com>
2017-09-29 15:38:38 +0200

    refactor: Add styles to `NewDeck` component

--- components/NewDeck.js
+++ components/NewDeck.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
-import { View, TouchableOpacity, Text } from 'react-native';
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import { white, black, grey } from '../utils/colors';
 
-// Create a lacal `SubmitButton` component.
+// Create a local `SubmitButton` component.
 function SubmitBtn({ onPress }) {
   return (
     <TouchableOpacity
-      onPress={onPress}
-    >
-      <Text>SUBMIT</Text>
+    	style={styles.submitBtn}
+      onPress={onPress}>
+      <Text style={styles.submitBtnText}>SUBMIT</Text>
     </TouchableOpacity>
-  )
+  );
+}
+
+function Question() {
+	return (
+		<View>
+			<Text style={styles.question}>
+				What is the title of your new deck?
+			</Text>
+		</View>
+	);
 }
 
 class NewDeck extends Component {
@@ -19,14 +30,44 @@ class NewDeck extends Component {
 
 	render() {
 		return (
-			<View>
-				<Text>New Deck</Text>
+			<View style={styles.container}>
+				<Question />
+				<Text style={{alignSelf: 'center'}}>FORM PLACEHOLDER</Text>
 				<SubmitBtn
 					onPress={this.submit}
 				/>
 			</View>
-		)
+		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		// flexDirection: 'row',
+		justifyContent: 'space-around'
+	},
+	submitBtn: {
+		backgroundColor: black,
+		padding: 10,
+		height: 45,
+		marginLeft: 100,
+		marginRight: 100,
+		borderRadius: 6,
+		// alignSelf: 'flex-end',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	submitBtnText: {
+		color: white,
+		fontSize: 22,
+		textAlign: 'center'
+	},
+	question: {
+		color: grey,
+		fontSize: 32,
+		textAlign: 'center'
+	}
+});
 
 export default NewDeck;
---
+++ utils/colors.js
@@ -0,0 +1,5 @@
+export const white = '#fff';
+export const black = '#000';
+export const grey = '#707070';
+export const red = '#f00';
+export const green = '#0f0';
