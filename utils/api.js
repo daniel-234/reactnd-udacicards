d766ab88bc61cec6843e7625d3ce2c89ed0f9af9
Daniele <daniele.rb6@gmail.com>
2017-10-08 18:28:24 +0200

    fix: Fix bug

--- utils/api.js
+++ utils/api.js
@@ -80,31 +80,31 @@ function createNotification() {
 }
 
 export function setLocalNotification() {
-	AsyncStorage.getItem(NOTIFICATION_KEY)
-		.then(JSON.parse)
-		.then((data) => {
-			if(data === null) {
-				Permissions.askAsync(Permissions.NOTIFICATIONS)
-					.then(({ status }) => {
-						if(status === 'granted') {
-							Notifications.cancelAllScheduledNotificationsAsync()
+  AsyncStorage.getItem(NOTIFICATION_KEY)
+    .then(JSON.parse)
+    .then((data) => {
+      if (data === null) {
+        Permissions.askAsync(Permissions.NOTIFICATIONS)
+          .then(({ status }) => {
+            if (status === 'granted') {
+              Notifications.cancelAllScheduledNotificationsAsync();
 
-							let tomorrow = new date()
-							tomorrow.setDate(tomorrow.getDate() + 1)
-							tomorrow.setHours(01)
-							tomorrow.setMinutes(00)
+              let tomorrow = new Date();
+              tomorrow.setDate(tomorrow.getDate() + 1);
+              tomorrow.setHours(18);
+              tomorrow.setMintutes(0);
 
-							Notifications.scheduleLocalNotificationAsync(
-								createNotification(),
-								{
-									time: tomorrow,
-									repeat: 'day'
-								}
-							)
+              Notifications.scheduleLocalNotificationAsync(
+                createNotification(),
+                {
+                  time: tomorrow,
+                  repeat: 'day',
+                }
+              )
 
-							AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
-						}
-					})
-			}
-		})
+              AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+            }
+          })
+      }
+    })
 }
